a381acf959427df545324a23d453ea224d70c098
vuiren <47295167+vuiren@users.noreply.github.com>
2019-09-16 22:22:47 +0500

    Update Task10.py

--- Task10.py
+++ Task10.py
@@ -1,8 +1,10 @@
 def IsPrime(x):
-    if(x==1): return False
-    if(x%2==0): return False
-    for n in range(3,x,2):
-        if(x % n == 0 and x!=n): return False;
+    if(x<=3): return x>1;
+    if(x%2==0 or x%3==0): return False;
+    i=5;
+    while(i*i<=x):
+        if(x % i==0 or x % (i+2)==0): return False;
+        i+=6;
     return True;
 
 def main():
